c33f0edd3ce0cbe2cff9d69089911de02ad5e408
Kaj Bromose <kbr@ucl.dk>
2020-09-16 14:07:04 +0200

    Initial commit

---
+++ OnionDemo.Database/BlogContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnionDemo.Domain.Model;
+
+namespace OnionDemo.Database
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ OnionDemo.Domain/Model/Post.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnionDemo.Domain.Model
+{
+    public class Post
+    {
+        /// <summary>
+        ///     Anvendes af Entity framework
+        /// </summary>
+        public Post()
+        {
+        }
+
+        public Post(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; }
+    }
+
+    public class Blog
+    {
+        private readonly List<Post> _posts;
+
+        /// <summary>
+        ///     Anvendes af Entity framework
+        /// </summary>
+        protected Blog()
+        {
+        }
+
+        public Blog(Guid id)
+        {
+            Id = id;
+            _posts = new List<Post>();
+        }
+
+        public Guid Id { get; }
+        public IEnumerable<Post> Posts => _posts;
+
+        public void AddPost(Post post)
+        {
+            _posts.Add(post);
+            Validate();
+        }
+
+        private void Validate()
+        {
+            // Max 10 posts
+            if (_posts.Count > 10) throw new MaxPostLimitExceeded($"Der er allerede {_posts.Count}");
+        }
+    }
+
+    public class MaxPostLimitExceeded : Exception
+    {
+        public MaxPostLimitExceeded(string message) : base(message)
+        {
+        }
+    }
+}
