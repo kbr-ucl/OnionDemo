ed132a06b438737d773861929839ecf6132f29db
Kaj Bromose <kbr@ucl.dk>
2020-09-22 20:49:27 +0200

    Query ajusted

--- Blog.Query/Model/PostDto.cs
+++ Blog.Query/Model/PostDto.cs
@@ -6,5 +6,9 @@ namespace Blog.Query.Model
     {
         public Guid Id { get; set; }
         public Guid BlogId { get; set; }
+
+        public string Body { get;  set; }
+
+        public string Title { get;  set; }
     }
 }
