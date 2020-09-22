7a9243a6b89c9f50c40479f0bc080b15cfa9706a
Kaj Bromose <kbr@ucl.dk>
2020-09-22 20:47:59 +0200

    Migration. And documentation

--- Blog.Database.Migrations/Migrations/20200922183716_Second-Iteration.Designer.cs
+++ Blog.Database.Migrations/Migrations/20200922183716_Second-Iteration.Designer.cs
@@ -4,14 +4,16 @@ using Blog.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Blog.Database.Migrations.Migrations
 {
     [DbContext(typeof(BlogContext))]
-    partial class BlogContextModelSnapshot : ModelSnapshot
+    [Migration("20200922183716_Second-Iteration")]
+    partial class SecondIteration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Blog.Database.Migrations/Migrations/20200922183716_Second-Iteration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blog.Database.Migrations.Migrations
+{
+    public partial class SecondIteration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Body",
+                table: "Posts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Posts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Body",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Posts");
+        }
+    }
+}
